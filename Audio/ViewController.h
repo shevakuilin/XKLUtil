042cd1707536902cdbea59aa47410a7f286722e6
shevakuilin <shevakuilin@gmail.com>
2016-07-26 16:12:00 +0800

    录音播放

---
+++ Audio/ViewController.h
@@ -0,0 +1,16 @@
+//
+//  ViewController.h
+//  AudioDemo
+//
+//  Created by tusm on 15/2/14.
+//  Copyright (c) 2015年 tusm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <AVFoundation/AVFoundation.h>
+
+@interface ViewController : UIViewController<AVAudioRecorderDelegate>
+
+
+@end
+
